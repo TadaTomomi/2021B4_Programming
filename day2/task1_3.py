87e494f754c9705c718c6b4149b433c164a8c907
TadaTomomi <82361163+TadaTomomi@users.noreply.github.com>
2021-04-19 18:31:25 +0900

    task1_3

---
+++ day2/task1_3.py
@@ -0,0 +1,17 @@
+import task1_1
+
+def xor(x1, x2):
+  p_nand = task1_1.Perceptron(-0.5, -0.7, -0.7)
+  t1 = p_nand.forward(x1, x2)
+  t2 = p_nand.forward(x1, t1)
+  t3 = p_nand.forward(t1, x2)
+  t4 = p_nand.forward(t2, t3)
+  return t4
+
+x1_list = [1, 1, 0, 0]
+x2_list = [1, 0, 1, 0]
+
+for i in range(4):
+  x1 = x1_list[i]
+  x2 = x2_list[i]
+  print("XOR(" + str(x1) + ", " + str(x2) +") = " + str(xor(x1, x2)))
