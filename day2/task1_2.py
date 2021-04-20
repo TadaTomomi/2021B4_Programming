d13ae990922c2107b8c2b170e4e1f6d697ce53c4
TadaTomomi <82361163+TadaTomomi@users.noreply.github.com>
2021-04-20 15:50:41 +0900

    task1_2

--- day2/task1_2.py
+++ day2/task1_2.py
@@ -7,9 +7,13 @@ p_or = task1_1.Perceptron(0.7, 0.7, 0.5)
 x1_list = [1, 1, 0, 0]
 x2_list = [1, 0, 1, 0]
 
-for i in range(4):
-  x1 = x1_list[i]
-  x2 = x2_list[i]
-  print("AND(" + str(x1) + ", " + str(x2) +") = " + str(p_and.forward(x1, x2)) + "  ", end="coding")
-  print("AND(" + str(x1) + ", " + str(x2) +") = " + str(p_nand.forward(x1, x2)) + "  ", end="coding")
-  print("AND(" + str(x1) + ", " + str(x2) +") = " + str(p_or.forward(x1, x2)))
+def main():
+  for i in range(4):
+    x1 = x1_list[i]
+    x2 = x2_list[i]
+    print("AND(" + str(x1) + ", " + str(x2) +") = " + str(p_and.forward(x1, x2)) + "  ", end="coding")
+    print("AND(" + str(x1) + ", " + str(x2) +") = " + str(p_nand.forward(x1, x2)) + "  ", end="coding")
+    print("AND(" + str(x1) + ", " + str(x2) +") = " + str(p_or.forward(x1, x2)))
+
+if __name__ == "__main__":
+  main()
